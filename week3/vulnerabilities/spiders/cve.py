ba00ba4f0ae237b6d2a875ab16cc0883341a8831
abuelnasr0 <abuelnasr70@gmail.com>
2024-02-07 05:00:07 +0200

    Download html file locally

--- week3/vulnerabilities/spiders/cve.py
+++ week3/vulnerabilities/spiders/cve.py
@@ -1,10 +1,14 @@
 import scrapy
+import os
 
+HTML_FILE = "source-EXPLOIT-DB.html"
+URL_PATH = os.path.join(os.getcwd(),HTML_FILE)
 
 class CveSpider(scrapy.Spider):
     name = "cve"
     allowed_domains = ["cve.mitre.org"]
-    start_urls = ["https://cve.mitre.org/data/refs/refmap/source-EXPLOIT-DB.html"]
+    # start_urls = ["https://cve.mitre.org/data/refs/refmap/source-EXPLOIT-DB.html"]
+    start_urls = [f"file://{URL_PATH}"]
 
     def parse(self, response):
         tables = response.xpath("//table")
@@ -27,21 +31,20 @@ class CveSpider(scrapy.Spider):
             if len(names) > 0 and len(id) > 0:
                 if not names[0].startswith("CVE"):
                     break
-                exploit_data["id"] = id[0]
+                exploit_data["id"] = id[0][10:]
                 exploit_data["names"] = names
                 expolits.append(exploit_data)
         
         for exploit in expolits[:10]:
             print(exploit)
-            
+
         # Example output
-        # {'id': 'EXPLOIT-DB:10102', 'names': ['CVE-2009-4186']}
-        # {'id': 'EXPLOIT-DB:1013', 'names': ['CVE-2005-1598']}
-        # {'id': 'EXPLOIT-DB:10168', 'names': ['CVE-2009-4767']}
-        # {'id': 'EXPLOIT-DB:10180', 'names': ['CVE-2009-4091', 'CVE-2009-4092', 'CVE-2009-4093']}
-        # {'id': 'EXPLOIT-DB:10183', 'names': ['CVE-2011-4906']}
-        # {'id': 'EXPLOIT-DB:10201', 'names': ['CVE-2009-4781']}
-        # {'id': 'EXPLOIT-DB:10216', 'names': ['CVE-2009-4223']}
-        # {'id': 'EXPLOIT-DB:10217', 'names': ['CVE-2009-4779']}
-        # {'id': 'EXPLOIT-DB:10218', 'names': ['CVE-2009-4082']}
-        # {'id': 'EXPLOIT-DB:10220', 'names': ['CVE-2009-4220']}
+        # {'id': ':10102', 'names': ['CVE-2009-4186']}
+        # {'id': ':1013', 'names': ['CVE-2005-1598']}
+        # {'id': ':10168', 'names': ['CVE-2009-4767']}
+        # {'id': ':10180', 'names': ['CVE-2009-4091', 'CVE-2009-4092', 'CVE-2009-4093']}
+        # {'id': ':10183', 'names': ['CVE-2011-4906']}
+        # {'id': ':10201', 'names': ['CVE-2009-4781']}
+        # {'id': ':10216', 'names': ['CVE-2009-4223']}
+        # {'id': ':10217', 'names': ['CVE-2009-4779']}
+        # {'id': ':10218', 'names': ['CVE-2009-4082']}
